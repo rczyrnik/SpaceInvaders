e1533763acd9e321a145a0cd6fa20cf77f1b01a6
rczyrnik <rczyrnik@gmail.com>
2017-05-19 05:49:37 -0400

    added bullet

--- GoodbyeSpace.py
+++ GoodbyeSpace.py
@@ -1,45 +1,52 @@
 '''
     Significant help from https://www.raywenderlich.com/24252/beginning-game-programming-for-teens-with-python
     Modified for my goals
+    
+    "a" = move left
+    "d" = move right
+    "f" = fire
     '''
 
 # 1 - Import library
 import pygame
+import math
 from pygame.locals import *
 
 # 2 - Initialize the game
 pygame.init()
 width, height = 640, 480
 screen=pygame.display.set_mode((width, height))
-keys = [False, False]
+keys = [False, False, False]
 playerpos=[width//2-22,height-45]
 invaders=[[30,10],[130,10],[230,10],[330,10],[430,10],[530,10]]
 left = True
 down = False
+acc=[0,0]
+arrows=[]
 
 # 3 - Load images
 player = pygame.image.load("resources/defense.png")
 space = pygame.image.load("resources/space.png")
 ground = pygame.image.load("resources/ground.png")
 invaderimg = pygame.image.load("resources/invader.png")
+arrow = pygame.image.load("resources/bullet.png")
 
 # 4 - keep looping through
 while 1:
     # 5 - clear the screen before drawing it again
     screen.fill(0)
     
-    
     # 6 - draw the screen elements
-    for x in range(width//space.get_width()+1):
+    for x in range(width//space.get_width()+1):         # background
         for y in range(height//space.get_height()+1):
             screen.blit(space,(x*300,y*300))
-    for x in range(width//ground.get_width()+1):
+    
+    for x in range(width//ground.get_width()+1):        # planet
         screen.blit(ground,(x*300,height-40))
-    screen.blit(player, playerpos)
 
-    #draws invaders
-    for invader in invaders:
+    for invader in invaders:                            # invaders
         screen.blit(invaderimg, invader)
+
 
     #updates y position of the invader, removes if off screen
     index=0
@@ -65,8 +72,28 @@ while 1:
         if down == True:
             invader[1]+=30
             down = False
-    
         index+=1
+
+
+    # 6.2 - Draw bullet
+    for bullet in arrows:
+        index=0
+        velocity = 3
+        bullet[2]-=velocity
+        if bullet[1]<-64 or bullet[1]>640 or bullet[2]<-64 or bullet[2]>480:
+            arrows.pop(index)
+        index+=1
+        for projectile in arrows:
+            arrow1 = pygame.transform.rotate(arrow, 360-projectile[0]*57.29)
+            screen.blit(arrow1, (projectile[1], projectile[2]))
+
+
+    # 6.1 - Set player position and rotation
+    position = pygame.mouse.get_pos()
+    angle = 0
+    playerrot = pygame.transform.rotate(player, 360-angle*57.29)
+    playerpos1 = (playerpos[0]-playerrot.get_rect().width/2, playerpos[1]-playerrot.get_rect().height/2)
+    screen.blit(playerrot, playerpos1)
 
 
 
@@ -87,13 +114,28 @@ while 1:
             keys[0]=True
         elif event.key==K_d:
             keys[1]=True
+        elif event.key==K_f:
+            keys[2]=True
+
     if event.type == pygame.KEYUP:
         if event.key==pygame.K_a:
             keys[0]=False
         elif event.key==pygame.K_d:
             keys[1]=False
+        elif event.key==pygame.K_f:
+            keys[2]=False
+
+
     # 9 - Move player
     if keys[0]:
         playerpos[0]-=5
     if keys[1]:
         playerpos[0]+=5
+    if keys[2]:
+        position=pygame.mouse.get_pos()
+        acc[1]+=1
+        arrows.append([0,playerpos1[0]+15,playerpos1[1]-15])
+
+
+
+
